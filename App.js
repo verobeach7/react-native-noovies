806bce4d1fdb1adf6a6e6b036147ff59dc61b250
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-02-23 19:13:11 +0900

    React Native Master: General Concepts(SplashScreen: Pre-load fonts, icons, APIs, etc)

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // pre-load fonts, call APIs, etc
+        // 강의의 startLoading과 동일하게 동작
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+      } catch (e) {
+        console.log(e);
+      } finally {
+        // 강의의 onFinish와 동일하게 동작
+        setReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) {
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView}>
+      <Text>We are done loading!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
