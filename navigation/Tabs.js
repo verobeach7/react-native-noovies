07edef0e729ea0d868dad380b2386f1f84d7441f
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-02-24 21:09:32 +0900

    React Native Master: General Concepts(Configuring the Navigation by using props)

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -7,8 +7,17 @@ import Search from "../screens/Search";
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
+  // 모든 Tab.Screen에 screenOptions를 사용하여 공통으로 스타일 적용 가능
+  <Tab.Navigator
+    initialRouteName="Search"
+    screenOptions={{ tabBarLabelStyle: { backgroundColor: "red" } }}
+  >
+    {/* 탭 단위로 다른 스타일을 적용할 때는 options를 이용 */}
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{ tabBarLabelStyle: { backgroundColor: "yellow" } }}
+    />
     <Tab.Screen name="Tv" component={Tv} />
     <Tab.Screen name="Search" component={Search} />
   </Tab.Navigator>
