f75046acc6181ebb04bfefff030d176c8bc7d6a9
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-02-25 18:52:15 +0900

    React Native Master: General Concepts(Styled Components)

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+// styled에 모두 포함되어 있어 아래 import가 불필요함
+// import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-// Stack으로 이동하기 위해 props를 이용
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
+
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    // 다른 navigator의 스크린으로 이동하려는 경우 아래와 같이 작업해야함
-    onPress={() => navigate("Stack", { screen: "Three" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title selected={false}>Movies</Title>
+    <Title selected={true}>Movies</Title>
+  </Btn>
 );
+
+/* // 1. javascript object notation을 이용하면 코드가 더러워짐
+// 2. 이를 해결하기 위해 StyleSheet을 이용하여 코드를 분리해주었으나 여전히 css가 아닌 javascript로 코딩을 해야함
+const styles = StyleSheet.create({
+  btn: { flex: 1, justifyContent: "center", alignItems: "center" },
+  text: {
+    color: "blue",
+  },
+}); */
 
 export default Movies;
