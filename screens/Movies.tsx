7078ad9a4a0694b4e1568b7b57e2c14c0ba08c69
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-02-26 13:33:54 +0900

    React Native Master: Movie App(Screen #2: react-native-swiper&StyledComponents)

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
-import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
-import Swiper from "react-native-web-swiper";
+import Swiper from "react-native-swiper";
 import { makeImgPath } from "../utils";
 import { BlurView } from "@react-native-community/blur";
 // import { BlurView } from "expo-blur";
@@ -15,8 +20,6 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGM2ZDYxZWM2ZjA5ODJlOWJhMTllYzRlYzAxMGE4ZCIsInN1YiI6IjYzYzEyMmNkMDVhNTMzMDA3YzdlOWYzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bhwjqiO2fNlXPuNRgRKM1aji0KXhiB1MSvA_apy9Pl0",
   },
 };
-
-const API_KEY = "f0c6d61ec6f0982e9ba19ec4ec010a8d";
 
 const Container = styled.ScrollView``;
 
@@ -32,11 +35,45 @@ const Loader = styled.View`
 
 const BgImg = styled.Image``;
 
-const Title = styled.Text``;
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+`;
+
+const Wraper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+
+const Overview = styled.Text`
+  margin-top: 10px;
+  color: rgba(255, 255, 255, 0.8);
+`;
+
+// Overview component 확장
+// styled(Overview)는 Overview가 가지는 모든 속성을 가지고 옴
+const Votes = styled(Overview)`
+  font-size: 12px;
+`;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movie">> = () => {
+  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
 
@@ -61,9 +98,12 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movie">> = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.5}
-        controlsEnabled={false}
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
@@ -73,11 +113,21 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movie">> = () => {
               source={{ uri: makeImgPath(movie.backdrop_path) }}
             />
             <BlurView
-              blurType="light"
+              blurType={isDark ? "dark" : "light"}
               blurAmount={5}
               style={StyleSheet.absoluteFill}
             >
-              <Title>{movie.original_title}</Title>
+              <Wraper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Votes>⭐️{movie.vote_average}/10</Votes>
+                  ) : null}
+                  <Overview numberOfLines={5}>{movie.overview}...</Overview>
+                  {/* <Overview>{movie.overview.slice(0, 90)}...</Overview> */}
+                </Column>
+              </Wraper>
             </BlurView>
           </View>
         ))}
