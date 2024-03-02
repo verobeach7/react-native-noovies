af47d76b4e9244435bfdf6ff80afe6a2dc68bf6a
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-03-02 22:03:46 +0900

    React Native Master: Movie App(How to change options of screen: setOptions, LinearGradient, absoluteFill)

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,12 +1,44 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import styled from "styled-components/native";
 import { Movie, TV } from "../api";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../utils";
+import { LinearGradient } from "expo-linear-gradient";
+import { BLACK_COLOR } from "../colors";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0 20px;
+`;
+
+const Background = styled.Image``;
+
+const Column = styled.View`
+  flex-direction: row;
+  width: 80%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0 20px;
 `;
 
 // 내가 생성하는 type
@@ -27,15 +59,31 @@ const Detail: React.FC<DetailScreenProps> = ({
   useEffect(() => {
     // setOptions method를 이용해 컴포넌트의 props, state, context를 사용하거나 바꿔줄 수 있음
     setOptions({
-      title:
-        "original_title" in params
-          ? params.original_title
-          : params.original_name,
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || "") }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["transparent", BLACK_COLOR]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
