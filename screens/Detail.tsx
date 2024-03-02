a479a38483e35d502f2a31b83e48a9bcb1a6be43
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-03-02 21:05:47 +0900

    React Native Master: Movie App(Make type of params for Navigation with TypeScript)

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,3 +1,4 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
@@ -6,9 +7,18 @@ const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
+// 내가 생성하는 type
+type RootStackParamList = {
+  // 스크린 이름과 스크린에서 사용하는 prop의 타입을 넣어주면 됨
+  Detail: { originalTitle: string };
+};
+
+// 어떤 Props를 이용하는지 골라줘야 함: NativeStackScreen인지 TabNavigationScreen인지 등
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
+
 // ES6 구조분해할당: 객체의 navigation에 해당하는 것과 나머지 것드로 나누어 받아옴
 // const Detail = ({ navigation, ...rest }) => {
-const Detail = ({
+const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: {
     params: { originalTitle },
